ac7a8a4357cd7dd90b8e7df667c5aa3238b6e8f8
apaixav <lucasrib01@gmail.com>
2023-09-27 11:18:18 -0300

    Task-03

---
+++ FelizMente/Model/Tema.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FelizMente.Model
+{
+    public class Tema
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string nome { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(510)]
+        public string descricao { get; set; } = string.Empty;
+    }
+}
---
+++ FelizMente/Validator/TemaValidator.cs
@@ -0,0 +1,25 @@
+﻿using FelizMente.Model;
+using FluentValidation;
+
+namespace FelizMente.Validator
+{
+    public class TemaValidator : AbstractValidator<Tema>
+    {
+        public TemaValidator()
+        {
+            RuleFor(t => t.nome)
+               .NotEmpty()
+               .MinimumLength(5)
+               .MaximumLength(255);
+
+
+            RuleFor(t => t.descricao)
+               .NotEmpty()
+               .MinimumLength(5)
+               .MaximumLength(510);
+
+        }
+
+    }
+      
+}
