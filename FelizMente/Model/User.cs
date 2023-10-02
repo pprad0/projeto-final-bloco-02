77a89c35d1eb31687c560404207c4fc669e2a2ba
Vitor Cesario <vitorsousa676@gmail.com>
2023-10-02 10:57:05 -0300

    Usuario

---
+++ FelizMente/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FelizMente.Model
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Usuario { get ; set; } = string.Empty;
+        [Column(TypeName = "varchar")]
+        [StringLength(510)]
+        public string Foto { get ; set; } = string.Empty;
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Senha { get ; set; } = string.Empty;
+        [Column(TypeName = "bit")]
+
+        public bool Tipo { get; set; }
+    }
+}
